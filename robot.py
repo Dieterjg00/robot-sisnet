408abc1f4dbdf7ef10b0f6d68dbfd795c3807554
Dieter Jungbluth <dieter.wjg@gmail.com>
2021-01-08 16:09:25 -0500

    prueba

--- robot.py
+++ robot.py
@@ -2,8 +2,10 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
 
+print('Llega inicial')
+
 driver = webdriver.Chrome(executable_path= r'./chromedriver.exe')
 
 driver.get('https://www.netpolizas.com/admin/login')
 
-print('Llega')
+print('Llega final')
